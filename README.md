d1a30925a95d6f37c131ab59f903c13836163ea0
Caleb Eby <caleb.eby01@gmail.com>
2016-12-21 14:39:18 -0800

    Merge boilerplate

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [SuitCSS](https://github.com/suitcss/preprocessor) support for [JSTransformers](http://github.com/jstransformers).
 
 [![Build Status](https://img.shields.io/travis/jstransformers/jstransformer-suitcss/master.svg)](https://travis-ci.org/jstransformers/jstransformer-suitcss)
-[![Coverage Status](https://img.shields.io/coveralls/jstransformers/jstransformer-suitcss/master.svg)](https://coveralls.io/r/jstransformers/jstransformer-suitcss?branch=master)
+[![Coverage Status](https://img.shields.io/codecov/c/github/jstransformers/jstransformer-suitcss/master.svg)](https://codecov.io/gh/jstransformers/jstransformer-suitcss)
 [![Dependency Status](https://img.shields.io/david/jstransformers/jstransformer-suitcss/master.svg)](http://david-dm.org/jstransformers/jstransformer-suitcss)
 [![NPM version](https://img.shields.io/npm/v/jstransformer-suitcss.svg)](https://www.npmjs.org/package/jstransformer-suitcss)
 
--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-'use strict';
+'use strict'
 
-var suitcss = require('suitcss-preprocessor');
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
+var suitcss = require('suitcss-preprocessor')
 
-exports.name = 'suitcss';
-exports.outputFormat = 'css';
+exports.name = 'suitcss'
+exports.outputFormat = 'css'
 
-exports.renderAsync = function (str, options, locals) {
+exports.renderAsync = function (str, options) {
   return new Promise(function (resolve, reject) {
     suitcss(str, options).then(function (result) {
       if (result && result.css) {
-        resolve(result.css);
+        resolve(result.css)
+      } else {
+        reject(result)
       }
-      else {
-        reject(result);
-      }
-    });
-  });
-};
+    })
+  })
+}
 
-exports.renderFileAsync = function (filename, options, locals) {
+exports.renderFileAsync = function (filename, options) {
   return new Promise(function (resolve, reject) {
     fs.readFile(filename, 'utf8', function (err, data) {
       if (err) {
-        reject(err);
-      }
-      else {
+        reject(err)
+      } else {
         options = options || {}
-        options.root = options.root || path.dirname(filename);
-        exports.renderAsync(data, options, locals).then(resolve).catch(reject);
+        options.root = options.root || path.dirname(filename)
+        exports.renderAsync(data, options).then(resolve).catch(reject)
       }
-    });
-  });
-};
+    })
+  })
+}
